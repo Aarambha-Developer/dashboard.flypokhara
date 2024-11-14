87450d95a6514cd8061c7a8c21aaf7e2ced806b7
adkshishir <adhikarishishir50@gmail.com>
2024-11-14 15:50:34 +0545

    feat: booking table

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,10 +1,12 @@
-import BookingForm from "@/components/booking-form/booking-form";
-import React from "react";
+import BookingForm from '@/components/booking-form/booking-form';
+import { FlightBookingsTableComponent } from '@/components/flight-bookings-table';
+import React from 'react';
 
 export default function Booking() {
   return (
-    <div className="mx-auto max-w-4xl flex justify-center items-center ">
+    <div className='mx-auto max-w-4xl  justify-center items-center '>
       <BookingForm />
+      <FlightBookingsTableComponent />
     </div>
   );
 }
