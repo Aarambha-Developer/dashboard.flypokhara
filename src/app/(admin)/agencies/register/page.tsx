37754a66f406a6f7acc449eff8b19e5fd07e1c9e
adkshishir <adhikarishishir50@gmail.com>
2024-11-19 11:56:57 +0545

    feat: new route for register agencies

---
+++ src/app/(admin)/agencies/register/page.tsx
@@ -0,0 +1,21 @@
+import { RegisterForm } from '@/components/registration-form';
+import { getCookie } from '@/lib/cookie-handler';
+import React from 'react';
+
+export default async function RegisterAgency() {
+  const role = await getCookie('role');
+
+  return (
+    <div>
+      {role === 'ADMIN' ? (
+        <RegisterForm role={role} />
+      ) : (
+        <div>
+          <h2 className='flex justify-center items-center text-lg text-red-500 mt-20 uppercase'>
+            Unauthorized page
+          </h2>{' '}
+        </div>
+      )}
+    </div>
+  );
+}
